288e635b14953a51672f8597cb0436d8645ff8ed
jgambard <jmichel.gambard@gmail.com>
2020-12-19 16:51:44 +0100

    fix l_test output / add ll_test in bonus

--- tests/l_test.cpp
+++ tests/l_test.cpp
@@ -9,6 +9,8 @@ extern "C"
 #include "check.hpp"
 #include "print.hpp"
 #define TEST_LIMIT 30
+#define ULONG_MIN ((unsigned long int)0)
+
 
 int iTest = 1;
 int testNumber;
@@ -18,7 +20,7 @@ int main(int ac, char ** av)
 {
 	signal(SIGSEGV, sigsegv);
 	#ifdef __unix__
-			setlocale(LC_ALL, "");
+		setlocale(LC_ALL, "");
 	#endif
 	#ifdef __APPLE__
 		setlocale(LC_ALL, "en_US");
@@ -28,8 +30,7 @@ int main(int ac, char ** av)
 	testName = av[0];
 	if (ac != 1)
 		printTestNumber(av[1], TEST_LIMIT);
-	cout << endl;
-	SUBCATEGORY(1, 4,  cout << FG_LGRAY << "subcategory: d" << RESET_ALL << endl;)
+	SUBCATEGORY(1, 4,  cout << endl << FG_LGRAY << "subcategory: d" << RESET_ALL << endl;)
 	TEST(1, print(" %ld ", INT_MAX + 1));
 	TEST(2, print(" %ld ", INT_MIN - 1));
 	TEST(3, print(" %ld ", LONG_MAX + 1));
@@ -40,17 +41,17 @@ int main(int ac, char ** av)
 	TEST(7, print(" %li ", LONG_MAX + 1));
 	TEST(8, print(" %li ", LONG_MIN - 1));
 	SUBCATEGORY(9, 11, cout << endl << FG_LGRAY << "subcategory: u" << RESET_ALL << endl;)
-	TEST(9, print(" %lu ", UINT_MAX));
-	TEST(10, print(" %lu ", UINT_MAX + 1));
-	TEST(11, print(" %lu ", -1));
+	TEST(9, print(" %lu ", ULONG_MAX));
+	TEST(10, print(" %lu ", ULONG_MAX + 1));
+	TEST(11, print(" %lu ", ULONG_MIN - 1));
 	SUBCATEGORY(12, 14, cout << endl << FG_LGRAY << "subcategory: x" << RESET_ALL << endl;)
-	TEST(12, print(" %lx ", UINT_MAX));
-	TEST(13, print(" %lx ", UINT_MAX + 1));
-	TEST(14, print(" %lx ", -1));
+	TEST(12, print(" %lx ", ULONG_MAX));
+	TEST(13, print(" %lx ", ULONG_MAX + 1));
+	TEST(14, print(" %lx ", ULONG_MIN - 1));
 	SUBCATEGORY(15, 17, cout << endl << FG_LGRAY << "subcategory: X" << RESET_ALL << endl;)
-	TEST(15, print(" %lX ", UINT_MAX));
-	TEST(16, print(" %lX ", UINT_MAX + 1));
-	TEST(17, print(" %lX ", -1));
+	TEST(15, print(" %lX ", ULONG_MAX));
+	TEST(16, print(" %lX ", ULONG_MAX + 1));
+	TEST(17, print(" %lX ", ULONG_MIN - 1));
 	SUBCATEGORY(18, 19, cout << endl << FG_LGRAY << "subcategory: n" << RESET_ALL << endl;)
 	TEST(18, checkn<long int>("%ln"));
 	TEST(19, checkn<long int>(" %ln "));
